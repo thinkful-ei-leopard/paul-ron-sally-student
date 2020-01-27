ac4a4e5439aa0aefb2366464e2da1cab3c2cbc2f
Ron Martin <ronmartin89@gmail.com>
2020-01-27 09:05:55 -0800

    Added all HTML and fixed the closing tags (ie br and img tags)

--- src/App.js
+++ src/App.js
@@ -36,6 +36,38 @@ function App() {
         <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
       </ul>
     </section>
+
+
+    <section>
+      <header>
+        <h2>Exhibitions</h2>
+      </header>
+      <ul>
+        <li>Two-Person Exhibition: Moore Contemporary<br />
+        (with Industrial artist Charlie Martin)<br />
+        New York, NY / August 2019</li>
+        <li>Solo Exhibition: Azeroth Contemporary<br />
+        Lahdeda Gallery<br />
+        Austin, TX / June 2017</li>
+        <li>Solo Exhibition: Thesis Exhibition<br />
+        Juan Diego Fine Art Gallery<br />
+        San Diego, CA / May 2015</li>
+      </ul>
+    </section>
+    <section>
+      <header>
+        <h2>Contact Information</h2>
+      </header>
+      <p>Sally Student</p>
+      <address>
+      Email me: <a href="mailto:sallystudent@fakemail.com">sallystudent@fakemail.com</a>
+      <br />
+      Call me: <a href="tel:=+17025555309">(702) 555-5309</a>
+      <br />
+      <img src="https://m.media-amazon.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_.jpg" alt="Photograph of a brunette woman with her arms crossed. She is wearing an orange sweater with zebras on it and blue jeans." />
+      </address>
+    </section>
+
   </div>
   );
 }
